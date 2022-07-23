76f65f5ad008a92e5a243a1dfec52451f3dd35dd
ediprodan <ediprodan@protonmail.com>
2022-07-24 00:11:25 +0200

    added routes for admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,19 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React from 'react';
 import './App.css';
-import ButtonExpand from './components/ButtonExpand';
-import Map, {Marker, FullscreenControl, GeolocateControl, NavigationControl, ScaleControl} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { mapTrashType } from './util/mapTrashType';
+import User from './components/User';
+import Admin from './components/Admin';
+import {BrowserRouter as Router, Route, Routes}from 'react-router-dom';
+
 
 function App() {
 
-  let [containers, setContainers] = useState([])
-
-  useEffect( () => {
-    fetch("http://localhost:8080/api/trash-containers", {
-            mode: 'cors',
-            headers: {
-              'Access-Control-Allow-Origin':'*'
-            }
-          })
-        .then(response => response.json())
-        .then(json => setContainers(json))
-  }, [])
-
   return (
-      <div className="container">  
-        <Map
-          initialViewState={{
-          longitude: 14.44,
-          latitude: 45.33,
-          zoom: 12
-          }}
-          style={{width: "100vw", height:"100vh"}}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          mapboxAccessToken="pk.eyJ1IjoiYm9qYW5wdXZhY2EiLCJhIjoiY2w1eHIydmpoMHdndzNibnBuOHA0OWtzcSJ9.9EKcXB_wGL918f5HDKd2mA">
-            <GeolocateControl position="top-left" />
-            <FullscreenControl position="top-left" />
-            <NavigationControl position="top-left" />
-            <ScaleControl />
-            {containers.map((container) =>
-              <Marker longitude={container.locationLong}
-                latitude={container.locationLat}
-                anchor="bottom"
-                key={container.id}
-                color={mapTrashType(container.trashType).color}
-               />
-            )}
-        </Map>
-        <ButtonExpand />
-      </div>
-
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<User />} />
+        <Route exact path='/admin' element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,16 @@
+import ButtonExpand from './ButtonExpand';
+import ViewMap from './ViewMap';
+
+import React from 'react'
+
+const User = () => {
+  return (
+
+    <div className='container'>
+        <ViewMap />
+        <ButtonExpand />
+        </div>
+  )
+}
+
+export default User
